df360a851eab409bf724c0265cae4f801afaae54
Jacky <jacky@amazingtalker.com>
2026-01-26 17:20:53 +0800

    bump version

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: IS_DEV ? "Attain (Dev)" : "Attain",
     slug: "coach-vocabulary-frontend",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/icon.png",
     scheme: IS_DEV ? "attain-dev" : "attain",
